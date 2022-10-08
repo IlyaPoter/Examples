290d42d06b3f7061b986c8d23a14254de9734e53
study <study@lan.sibadi.org>
2022-10-09 00:15:05 +0600

    add example 13

---
+++ Example013/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = new Random().Next(100, 1000);
+
+int firstSymbol = num / 100;
+int lastSymbol = num % 10;
+
+Console.WriteLine($"Число {num} стало {firstSymbol}{lastSymbol}");
---
+++ Example014/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите певрое число: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine());
+if (firstNumber == 0)
+{
+    Console.WriteLine("Error");
+}
+else
+{
+    int result = secondNumber / firstNumber;
+    if (result == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine(result);
+    }
+}
