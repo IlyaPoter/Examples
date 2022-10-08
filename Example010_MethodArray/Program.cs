62053957c091c4c413aee389669057b87d60ce6b
study <study@lan.sibadi.org>
2022-10-08 22:39:40 +0600

    finish Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 1, 17, 33, 4, 55, 6, 17, 18 };
+int n = array.Length;
+int find = 17;
+int index = 0;
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
