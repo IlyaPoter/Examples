a3211f32210c729a57e25f73ce78ff8c6b625fa0
study <study@lan.sibadi.org>
2022-10-09 00:30:55 +0600

    finish example 015

---
+++ Example015/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%7 ==0 && number%23 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else{
+        Console.WriteLine("Не кратно");
+}
