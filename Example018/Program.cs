50a4b6665360f10c56ecf4b46515af65fa5aeaa7
study <study@lan.sibadi.org>
2022-10-09 20:04:29 +0600

    add example 018

---
+++ Example018/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти: ");
+int qurd= int.Parse(Console.ReadLine());
+if (qurd==1)
+{
+    Console.WriteLine("X>0 and Y>0");
+}
+if (qurd==2)
+{
+    Console.WriteLine("X<0 and Y>0");
+}
+if (qurd==3)
+{
+    Console.WriteLine("X<0 and Y<0");
+}
+if (qurd==4)
+{
+    Console.WriteLine("X>0 and Y<0");
+}
+else
+{
+    Console.WriteLine("Не корректно введена четверь");
+}
