8ea863ff72406d8dd19954c3a9cf26b67087837e
study <study@lan.sibadi.org>
2022-10-09 20:20:46 +0600

    add example 019

---
+++ Example019/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите координату Х первого числа: ");
+int oneX = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y первого числа: ");
+int oneY = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Х второго числа: ");
+int twoX = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координату Y второго числа: ");
+int twoY = int.Parse(Console.ReadLine());
+int differentX = oneX - twoX;
+int differentY = oneY - twoY;
+double result = Math.Sqrt((differentX*differentX+differentY*differentY));
+Console.WriteLine(result);
+
+
+
