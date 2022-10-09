3f2030a7708539ea393dbaeec630538b8d8e4c63
study <study@lan.sibadi.org>
2022-10-09 13:19:10 +0600

    add example 15 and 16

---
+++ Example016/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Является");
+}
+else if (number2 == number1 * number1)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
+
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Является");
+//     return;
+// }
+// if (number2 == number1 * number1)
+// {
+//     Console.WriteLine("Является");
+//     return;
+// }
+// Console.WriteLine("Не является");
