3b3b9ad93a95121be578734bd197d0f8939b0de4
study <study@lan.sibadi.org>
2022-10-09 19:48:47 +0600

    add example 017

---
+++ Example017/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Clear();
+Console.WriteLine("Введи Х: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи Y: ");
+int y = int.Parse(Console.ReadLine());
+if (x>0)
+{
+    if (y>0)
+    {
+        Console.WriteLine("1 четверть");
+    }
+    else
+    {
+        Console.WriteLine("4 четверь");
+    }
+}
+else
+{
+    if (y>0)
+    {
+        Console.WriteLine("2 четверь");
+    }
+    else
+    {
+        Console.WriteLine("3 четверть");
+    }
+}
+
+
