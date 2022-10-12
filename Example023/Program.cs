48e2f774975e488e2d956f4aefb529d9abad54d3
study <study@lan.sibadi.org>
2022-10-12 22:58:27 +0600

    add example 023

---
+++ Example023/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int num = int.Parse(Console.ReadLine());
+int result = 1;
+int Multi(int number)
+{
+    for (int count = 1; count <= number; count++)
+    {
+        result = result * count;
+    }
+    return result;
+}
+Console.WriteLine(Multi(num));
