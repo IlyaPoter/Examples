36a19590d1de5282cfa945eaf6448c1b3da174cb
study <study@lan.sibadi.org>
2022-10-12 22:35:04 +0600

    add example 22

---
+++ Example 022/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int result = 0;
+
+int Numbers(int number)
+{
+    for (result = 0; number>0; result++)
+    {
+        number = number / 10;
+    }
+
+    // while (number > 0)
+    // {
+    //     number = number / 10;
+    //     result++;
+    // }
+    return result;
+}
+Console.WriteLine(Numbers(num));
