875bdbea031f84591b460cfd3645109fa47b716a
study <study@lan.sibadi.org>
2022-10-13 23:34:44 +0600

    add example 024

---
+++ Example 024/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+
+Console.Clear();
+
+int[] array = new int[8];
+
+void MethodArray(int[] newarray)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        newarray[i] = new Random().Next(0,2);
+        Console.Write($"{newarray[i]} ");
+    }
+}
+
+MethodArray(array);
