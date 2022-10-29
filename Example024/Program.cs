eb3d9b98d9f8d64ca711d0c3a4db343bc1f8ec58
study <study@lan.sibadi.org>
2022-10-29 20:28:10 +0600

    add example24

---
+++ Example024/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+int[] array = GetArray(16, 0, 100);
+PrintArray(array);
+//ReversArray(array);
+Console.WriteLine();
+//PrintArray(array);
+Console.WriteLine();
+int[] reversArray = ReversArray2(array);
+PrintArray(reversArray);
+
+
+void ReversArray(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - 1 - i];
+        inArray[inArray.Length - 1 - i] = k;
+    }
+}
+
+int[] ReversArray2(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
