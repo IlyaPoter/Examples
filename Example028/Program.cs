6b9cf3299385e800de98394be4ef13f4efdb708a
study <study@lan.sibadi.org>
2022-10-30 18:39:22 +0600

    add example028

---
+++ Example028/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования
+
+
+
+void Main()
+{
+    Console.Clear();
+    int[] arr = { 1, 2, 3, 4, 5 };
+    int[] CopyArray = GetArrCopy(arr);
+    WriteArray(CopyArray);
+}
+
+int[] GetArrCopy(int[] arr)
+{
+    int[] newArray = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArray[i] = arr[i];
+    }
+    return newArray;
+}
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+Main();
